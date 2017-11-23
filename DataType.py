b524be76a8c4dc9a74061142cd930c36c0a06d80
chenxf <chenxf@13322.com>
2017-11-23 14:00:05 +0800

    add DataType

---
+++ DataType.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 字符串
+print('hello world')
+# 整数
+print(1)
+# 浮点数
+print(1.23)
+# 转移字符  \n 换行 \t制表符  \\ --> \
+print("字符串中添加\'单引号\'并输出")
+print("字符串中添加\"单引号\"并输出")
+# 布尔值
+print('布尔值输出:', 3 > 5)
+
+# 布尔值可以用and与运算 or或运算  not运算
+score = 98
+if score > 60:
+    print('优秀')
+else:
+    print('挂科')
+
+# 空值为None
+
+# 变量
+a = 1
+b = 'abc'
+c = 1.1333
+
+print(a, b, c)
+# 常量大写
+PI = 3.1415926
